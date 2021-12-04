0c9ff19bf855bbbb37afbc29c82fda8c1b1b9e2b
Kevyn Marins <kevynmarins@gmail.com>
2021-12-04 17:14:40 +0000

    Login done

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const db = require("../models/nedb");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -27,6 +28,41 @@ exports.register = async (req, res) => {
     }
 }
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
+    console.log("Inside login function");
+    if(!req.body) {
+        return res.status(400).send({
+            message: "Content cannot be empty!"
+        });
+    }
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const email = req.body.email;
+        const password = hashPassword;
     
-}
+        db.hasEmail(email)
+        .then( async (data) => {
+            if(data == null) {
+                console.log("User not found!");
+                return res.status(401).send({message: "User not found!"});
+            }
+    
+            if(await bcrypt.compare(req.body.password, data.password)) {
+                const user = {email: email};
+                const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    
+                res.json({acessToken: acessToken});
+                console.log("Data: ");
+                console.log(JSON.stringify(data));
+            }
+            else {
+                console.log("Incorrect Password");
+                return res.status(401).send({ error: "Incorrect Password"});
+            }
+        });
+    }
+    catch {
+        return res.status(400).send({message: data});
+    }
+};
--- public/js/user.js
+++ public/js/user.js
@@ -1,16 +1,13 @@
 const urlBase = window.location.origin;
 
 function register() {
+    console.log("Inside user.register()");
     const email = document.querySelector("#emailRegister").value;
     const password = document.querySelector("#passwordRegister").value;
-    const status = document.querySelector("#statusRegister");
-    
-    console.log("urlBase: ");
-    console.log(urlBase);
+    const status = document.querySelector("#statusRegister");    
 
     if(password.length < 4) {
-        status.innerHTML = `
-            Password must be at least 4 characters long.`;
+        status.innerHTML = "Password must be at least 4 characters long.";
         return;
     }
 
@@ -37,5 +34,36 @@ function register() {
 }
 
 function login() {
+    console.log("Inside user.login()");
+    const email = document.querySelector("#emailLogin").value;
+    const password = document.querySelector("#passwordLogin").value;
+    const status = document.querySelector("#statusLogin");    
 
-}
+    if(password.length < 4) {
+        status.innerHTML = "Password must be at least 4 characters long.";
+        return;
+    }
+
+    fetch(`${urlBase}/login`, {
+        headers: {
+            "content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST",
+        body: `email=${email}&password=${password}`
+    })
+    .then(async (response) => {
+        if(!response.ok) {
+            const error = response.statusText;
+            status.innerHTML = error;
+            throw new Error(error);
+        }
+        const result = await response.json();
+        const token = result.acessToken;
+        console.log(token);
+        localStorage.setItem("token", token);
+        status.innerHTML = "Success!";
+    })
+    .catch((error) => {
+        status.innerHTML = `Error: ${error}`;
+    });
+};
